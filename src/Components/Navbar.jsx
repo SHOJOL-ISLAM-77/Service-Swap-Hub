addb036e5f011f2dbb18dfd973909b478c4da609
SHOJOL-ISLAM-77 <sh0j0l3875891@gmail.com>
2023-11-05 20:39:37 +0600

    add a navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,141 @@
+/* eslint-disable react/prop-types */
+import { useContext, useEffect, useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
+import logo from "../assets/logo.png"
+
+
+const Navbar = ({ children }) => {
+    const { user, logOut } = useContext(AuthContext);
+    const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+    const navigate = useNavigate()
+    const photo = user?.photoURL;
+    const name = user?.displayName;
+
+    const handleToggle = (e) => {
+        if (e.target.checked) {
+            setTheme("dark");
+        }
+        else {
+            setTheme("light");
+        }
+    }
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+        const localTheme = localStorage.getItem("theme");
+        document.querySelector("html").setAttribute("data-theme", localTheme);
+    }, [theme])
+
+    const handleSingOut = () => {
+        logOut()
+            .then(
+                navigate('/')
+            )
+            .catch()
+    }
+
+    const navLinks = (
+        <>
+            <div className="hidden lg:block">
+                <label className="swap swap-rotate items-center mr-5 ">
+                    <input type="checkbox" onChange={handleToggle} />
+
+                    <svg id="white" className="swap-on fill-current w-10 h-10 text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                    </svg>
+                    <svg id="dirk" className="swap-off fill-current w-10 h-10 text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                    </svg>
+                </label>
+            </div>
+            <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "mr-5 italic pb-1 border-b-[1px] border-black" : "mr-5 lg:my-0 my-3"} to="/">
+                Home
+            </NavLink>
+            <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active mr-5 italic pb-1 border-b-[1px] border-black" : "mr-5 lg:my-0 my-3"} to="/services">
+                Services
+            </NavLink>
+
+            {user ? (
+                <>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className=" mr-5 pb-1 border-base-100 focus:border-black">Dropdown</label>
+                        <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                            <li><Link to="/myServices">My Service</Link></li>
+                            <li><Link to="/addServices">Add Service</Link></li>
+                            <li><Link to="/addSchedules">Add Schedules</Link></li>
+                        </ul>
+                    </div>
+
+                    <button onClick={handleSingOut} className="mr-5 lg:my-0 my-3">Log Out </button>
+                </>
+            ) : (
+                <NavLink className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active mr-5 italic pb-1 border-b-[1px] border-black" : "mr-5 lg:my-0 my-3"} to="/login" >
+                    <p className="inline-flex items-center gap-x-2 font-medium sm:border-l sm:border-white/[.3]">
+                        <svg className="w-4 h-4 inline" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                        </svg>
+                        Log in
+                    </p>
+                </NavLink>
+            )}
+        </>
+    );
+
+
+
+    return (
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+
+                <div className="w-full navbar  container mx-auto">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                    </div>
+                    <div className="flex-1 px-2 mx-2  ">
+                        <div className="flex items-center gap-12">
+                            <Link to='/'><img className="h-[90px]" src={logo} alt="" /></Link>
+                            <div className="lg:hidden block">
+                                <label className="swap swap-rotate items-center mr-5 ">
+                                    <input type="checkbox" onChange={handleToggle} />
+
+                                    <svg id="white" className="swap-on fill-current w-10 h-10 text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                                    </svg>
+                                    <svg id="dirk" className="swap-off fill-current w-10 h-10 text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" >
+                                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                                    </svg>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-none hidden lg:flex  items-center ">
+                        {navLinks}
+                        {
+                            user && <div className="flex items-center">
+                                <div className="avatar mr-5">
+                                    <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                        <img src={photo} />
+                                    </div>
+                                </div>
+                                <p>{name}</p>
+                            </div>
+
+                        }
+                    </div>
+                </div>
+                {children}
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                    {navLinks}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
