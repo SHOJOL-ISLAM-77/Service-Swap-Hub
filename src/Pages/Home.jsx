022a0c51b35b41b1fc708e08467bf361799824cd
SHOJOL-ISLAM-77 <sh0j0l3875891@gmail.com>
2023-11-05 20:36:37 +0600

    add home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../Components/Banner";
+import OurPopularProduct from "../Components/OurPopularProduct";
+
+
+const Home = () => {
+    return (
+        <div>
+           <Banner/>
+           <OurPopularProduct/>
+        </div>
+    );
+};
+
+export default Home;
