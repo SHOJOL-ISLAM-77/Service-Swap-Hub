7b712304a35c7e4bcd8b1ec15cf28d5280623826
SHOJOL-ISLAM-77 <sh0j0l3875891@gmail.com>
2023-11-05 20:42:42 +0600

    add our populer product section

---
+++ src/Components/OurPopularProduct.jsx
@@ -0,0 +1,11 @@
+
+
+const OurPopularProduct = () => {
+    return (
+        <div className="container mx-auto my-[100px]">
+            <h2 className="text-4xl font-bold">Popular services</h2>
+        </div>
+    );
+};
+
+export default OurPopularProduct;
